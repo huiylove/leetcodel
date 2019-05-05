33d8cd4d1e98ade35ca4537184a1240b3e0037f6
huiylove <huiy6.2991@aliyun.com>
2019-05-05 11:16:59 +0800

    第二次提交

--- src/main/java/com/huiy/leetcodel/App.java
+++ src/main/java/com/huiy/leetcodel/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "Hello World!huiylove fighting!!!!yyyy" );
-=======
         System.out.println( "Hello World!huiylove fighting!!!!" );
->>>>>>> 13996d3e1abaeb2d05f5a20eaa02844da647f22d
     }
 }
